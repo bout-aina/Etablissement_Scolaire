28934e43759e0202b406260cd7194dc7192afa93
naour boutaina <naour_boutaina@emsi-edu.ma>
2022-08-05 18:28:36 +0200

    partie statistics

--- my-first-project/src/app/dashboard/dashboard.component.ts
+++ my-first-project/src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {ModuleService} from "../services/module.service";
 import {EtudiantService} from "../services/etudiant.service";
+import {ProfsService} from "../services/profs.service";
+import {EventService} from "../services/event.service";
 
 @Component({
   selector: 'app-dashboard',
@@ -10,20 +12,35 @@ import {EtudiantService} from "../services/etudiant.service";
 export class DashboardComponent implements OnInit {
 
   constructor(private etdService: EtudiantService,
-              private  moduleService : ModuleService) { }
-  lengthEtd : number = 0;
-  lengthModule : number =0;
+              private moduleService: ModuleService,
+              private profService: ProfsService,
+              private  eventService : EventService) {
+  }
+
+  lengthEtd: number = 0;
+  lengthModule: number = 0;
+  lengthProf: number = 0;
+  lengthActivity: number = 0;
+
   ngOnInit(): void {
-    this.etdService.etudiantlist().subscribe((data:any)=>{
+    this.etdService.etudiantlist().subscribe((data: any) => {
       this.lengthEtd = data.length;
       //console.log(data.length)
     })
-    this.moduleService.getModules().subscribe((data:any)=>{
+    this.moduleService.getModules().subscribe((data: any) => {
       this.lengthModule = data.length;
       //console.log(data.length)
     })
+
+    this.profService.getProfs().subscribe((data: any) => {
+      this.lengthProf = data.length;
+      //console.log(data.length)
+    })
+    this.eventService.eventlist().subscribe((data: any) => {
+      this.lengthActivity = data.length;
+      //console.log(data.length)
+    })
+
+
   }
-
-
-
 }

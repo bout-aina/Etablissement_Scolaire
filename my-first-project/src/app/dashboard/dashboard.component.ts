06bfd103071bf739abcb4ee419d5d38d711ac8a5
naour boutaina <naour_boutaina@emsi-edu.ma>
2022-08-05 18:03:23 +0200

    partie dashboard

--- my-first-project/src/app/dashboard/dashboard.component.ts
+++ my-first-project/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ModuleService} from "../services/module.service";
+import {EtudiantService} from "../services/etudiant.service";
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private etdService: EtudiantService,
+              private  moduleService : ModuleService) { }
+  lengthEtd : number = 0;
+  lengthModule : number =0;
   ngOnInit(): void {
+    this.etdService.etudiantlist().subscribe((data:any)=>{
+      this.lengthEtd = data.length;
+      //console.log(data.length)
+    })
+    this.moduleService.getModules().subscribe((data:any)=>{
+      this.lengthModule = data.length;
+      //console.log(data.length)
+    })
   }
+
+
 
 }
